8d1b27894b37cfa8fb8a957ee0a61ca864d0d3dc
Gabriela Zanon dos Santos <gabrielazsantos02@gmail.com>
2023-08-15 10:52:29 -0300

    Primeiro Commit do projeto springboot

---
+++ src/main/java/br/sc/senac/dw/exemplosdw/SenacDw20232BackendApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dw.exemplosdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacDw20232BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenacDw20232BackendApplication.class, args);
+	}
+
+}
